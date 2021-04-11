8060d823711fcbcde334582269d85f9874da6434
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-04-12 07:35:02 +0900

    管理ツールバー

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -2,19 +2,18 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
-    <title>あきなのブログ</title>
+    <?php get_header(); ?>
 </head>
 
 <body>
-    <?php get_header(); ?>
+    <?php get_template_part('includes/header'); ?>
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
             <p><a href="<?php the_permalink() ?>"><?php the_title() ?></a><br><?php the_excerpt() ?><br><?php the_post_thumbnail(); ?></p>
         <?php endwhile; ?>
     <?php endif; ?>
+
+    <?php wp_footer(); ?>
 </body>
 
 </html>
