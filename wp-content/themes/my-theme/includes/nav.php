71d4d71a9884d4e56dafdb0b24b1ad7d5b75c83c
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-04-12 21:26:22 +0900

    ナビゲーションバー作成

---
+++ wp-content/themes/my-theme/includes/nav.php
@@ -0,0 +1,14 @@
+<nav>
+    <ul class='nav'>
+        <li><a href="">プログラミング</a></li>
+        <li>
+            <label for="menu_bar">ワーホリ</label>
+            <input type="checkbox" id="menu_bar" />
+            <ul class="links">
+                <li><a href="">カナダ</a></li>
+                <li><a href="">オーストラリア</a></li>
+            </ul>
+        </li>
+        <li><a href="">モノ</a></li>
+    </ul>
+</nav>
--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -6,15 +6,18 @@
 </head>
 
 <body>
-    <?php get_template_part('includes/header'); ?>
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-            <p><?php the_excerpt() ?><br><?php the_post_thumbnail(); ?></p>
-        <?php endwhile; ?>
-    <?php endif; ?>
+    <div id="container">
+        <?php get_template_part('includes/header'); ?>
+        <?php get_template_part('includes/nav'); ?>
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+                <p><?php the_excerpt() ?><br><?php the_post_thumbnail(); ?></p>
+            <?php endwhile; ?>
+        <?php endif; ?>
 
-    <?php wp_footer(); ?>
+        <?php wp_footer(); ?>
+    </div>
 </body>
 
 </html>
