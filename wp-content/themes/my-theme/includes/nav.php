d63bf68cf47a5cc907a6817746a35338b6288ee3
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-04-17 23:38:36 +0900

    ナビバー をbootstrapに変更

--- wp-content/themes/my-theme/includes/nav.php
+++ wp-content/themes/my-theme/includes/nav.php
@@ -1,13 +1,15 @@
-<nav>
-    <ul class='nav'>
-        <li><a href="/blog/category/programming">プログラミング</a></li>
-        <li class='dropdown'>
-            <a href="#">ワーホリ</a>
-            <ul id="links">
-                <li><a href="/blog/category/working-holiday/canada">カナダ</a></li>
-                <li><a href="/blog/category/working-holiday/australia">オーストラリア</a></li>
-            </ul>
-        </li>
-        <li><a href="/blog/category/goods">モノ</a></li>
-    </ul>
+<nav class="navbar navbar-expand-lg">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-0">
+            <li class='nav-item'><a class="nav-link" href="/blog/category/programming">プログラミング</a></li>
+            <li class='nav-item dropdown'><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id='navbarDropdown' aria-haspopup="true" aria-expanded="false" role="button">ワーホリ</a>
+                <div class='dropdown-menu' aria-labelledby="navbarDropdown">
+                    <a class='dropdown-item' href="/blog/category/working-holiday/canada">カナダ</a>
+                    <div class='dropdown-divider'></div>
+                    <a class='dropdown-item' href="/blog/category/working-holiday/australia">オーストラリア</a>
+                </div>
+            </li>
+            <li class='nav-item'><a class="nav-link" href="/blog/category/goods">モノ</a></li>
+        </ul>
+    </div>
 </nav>
