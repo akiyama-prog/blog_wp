d9aa27606627b0f2a5abc21d5b7f9cd3055c8eba
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-04-18 00:07:31 +0900

    トップ画像の高さ調整

--- wp-content/themes/my-theme/includes/nav.php
+++ wp-content/themes/my-theme/includes/nav.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-expand-lg">
     <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
         <ul class="navbar-nav mr-0">
-            <li class='nav-item'><a class="nav-link" href="/blog/category/programming">プログラミング</a></li>
-            <li class='nav-item dropdown'><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id='navbarDropdown' aria-haspopup="true" aria-expanded="false" role="button">ワーホリ</a>
+            <li class='nav-item'><a class="nav-link" href="/blog/category/programming">Programming</a></li>
+            <li class='nav-item dropdown'><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id='navbarDropdown' aria-haspopup="true" aria-expanded="false" role="button">Working Holiday</a>
                 <div class='dropdown-menu' aria-labelledby="navbarDropdown">
-                    <a class='dropdown-item' href="/blog/category/working-holiday/canada">カナダ</a>
+                    <a class='dropdown-item' href="/blog/category/working-holiday/canada">Canada</a>
                     <div class='dropdown-divider'></div>
-                    <a class='dropdown-item' href="/blog/category/working-holiday/australia">オーストラリア</a>
+                    <a class='dropdown-item' href="/blog/category/working-holiday/australia">Australia</a>
                 </div>
             </li>
-            <li class='nav-item'><a class="nav-link" href="/blog/category/goods">モノ</a></li>
+            <li class='nav-item'><a class="nav-link" href="/blog/category/goods">mono</a></li>
         </ul>
     </div>
 </nav>
