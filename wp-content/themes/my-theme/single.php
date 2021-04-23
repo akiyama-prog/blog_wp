5e063f3c5adedf1485922ae03cbdaeb07f28d387
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-04-23 22:34:24 +0900

    ブログタイトル

--- wp-content/themes/my-theme/single.php
+++ wp-content/themes/my-theme/single.php
@@ -7,15 +7,17 @@
 
 <body>
     <?php get_template_part('includes/header'); ?>
-    <div id='indexPosts'>
-        <main>
-            <?php if (have_posts()) : ?>
-                <?php while (have_posts()) : the_post(); ?>
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <div class='singlePostTitle'>
+                <p><?php the_time('Y-m-d'); ?></p>
+                <h2><?php the_title(); ?></h2>
+                <p class='category'><?php $cat = get_the_category();
+                                    echo $cat[0]->cat_name; ?></p>
+            </div>
+            <div id='indexPosts'>
+                <main>
                     <article class='single-post'>
-                        <h2><?php the_title(); ?></h2>
-                        <p><?php the_time('Y-m-d'); ?></p>
-                        <p><?php $cat = get_the_category();
-                            echo $cat[0]->cat_name; ?></p>
                         <?php
                         if (has_post_thumbnail()) :
                             the_post_thumbnail('thumbnail', 'class=thumbnail');
@@ -27,14 +29,14 @@
                             <?php the_content() ?>
                         </p>
                     </article>
-                <?php endwhile; ?>
-            <?php endif; ?>
-        </main>
-        <!-- サイドバー -->
-        <aside>
-            <div></div>
-        </aside>
-    </div>
+                </main>
+                <!-- サイドバー -->
+                <aside>
+                    <div></div>
+                </aside>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
 </body>
 
 </html>
