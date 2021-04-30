d544f01bfc21bfba3263c7c2e1fa1100f51eb2cd
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-04-30 21:04:03 +0900

    facebook button 修正

--- wp-content/themes/my-theme/includes/sns.php
+++ wp-content/themes/my-theme/includes/sns.php
@@ -7,8 +7,7 @@ $title_encode = urlencode(get_the_title()) . '｜' . get_bloginfo('name');
     <ul>
         <!--Facebookボタン-->
         <li class="facebooklink">
-            <a href="//www.facebook.com/sharer.php?src=bm&u=<?php echo $url_encode; ?>&t=<?php echo $title_encode; ?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;">
-
+            <a href="http://www.facebook.com/share.php?u=<?php echo $url_encode; ?>&t=<?php echo $title_encode; ?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;">
                 <i class="fa fa-facebook"></i><span> facebook</span>
             </a>
         </li>
