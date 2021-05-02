bc666928f5fd8c29df698276369ab3cc700c26b1
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-05-02 12:01:35 +0900

    ptag削除

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -25,9 +25,7 @@
                                 ?>
                                     <img src="<?php echo get_template_directory_uri(); ?>/img/no-image.png" alt="No Image" class='thumbnail' />
                                 <?php endif; ?>
-                                <p>
-                                    <?php the_excerpt() ?>
-                                </p>
+                                <?php the_excerpt() ?>
                             </a>
                         </article>
                     <?php endwhile; ?>
