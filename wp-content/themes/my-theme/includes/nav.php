f0040fc544e0c620cf6adfc7a00c36ed323207a0
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-05-04 09:20:05 +0900

    travelに変更

--- wp-content/themes/my-theme/includes/nav.php
+++ wp-content/themes/my-theme/includes/nav.php
@@ -5,7 +5,7 @@
     <div class="collapse navbar-collapse justify-content-end" id="nav-bar">
         <ul class="navbar-nav mr-0">
             <li class='nav-item'><a class="nav-link" href="/blog/category/programming">Programming</a></li>
-            <li class='nav-item dropdown'><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id='navbarDropdown' aria-haspopup="true" aria-expanded="false" role="button">Working Holiday</a>
+            <li class='nav-item dropdown'><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id='navbarDropdown' aria-haspopup="true" aria-expanded="false" role="button">Travel</a>
                 <div class='dropdown-menu' aria-labelledby="navbarDropdown">
                     <a class='dropdown-item' href="/blog/category/working-holiday/canada">Canada</a>
                     <div class='dropdown-divider'></div>
