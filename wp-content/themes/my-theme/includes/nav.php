a4baae0f3aed7c3693e6d6bb358748b24112c0a9
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-05-04 09:07:52 +0900

    ハンバーガーメニュー作成

--- wp-content/themes/my-theme/includes/nav.php
+++ wp-content/themes/my-theme/includes/nav.php
@@ -1,5 +1,8 @@
-<nav class="navbar navbar-expand-lg">
-    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+<nav class="navbar navbar-expand-sm">
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="nav-bar">
         <ul class="navbar-nav mr-0">
             <li class='nav-item'><a class="nav-link" href="/blog/category/programming">Programming</a></li>
             <li class='nav-item dropdown'><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id='navbarDropdown' aria-haspopup="true" aria-expanded="false" role="button">Working Holiday</a>
