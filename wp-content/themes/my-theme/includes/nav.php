563694b9b6470509dc0360bd447f98e411a80cd7
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-05-13 19:31:38 +0900

    ハンバーガーメニュー作成

--- wp-content/themes/my-theme/includes/nav.php
+++ wp-content/themes/my-theme/includes/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-sm">
+<nav class="navbar navbar-expand-sm navbar-light">
     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -16,3 +16,13 @@
         </ul>
     </div>
 </nav>
+
+<style>
+    .navbar-light .navbar-toggler-icon {
+        background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255, 255, 255, 1%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
+    }
+
+    .navbar-light .navbar-nav .nav-link {
+        color: white;
+    }
+</style>
