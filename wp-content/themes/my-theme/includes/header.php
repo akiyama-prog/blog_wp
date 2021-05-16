484726469ea021b1c96ab2a0e2a18466d10609c1
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-05-16 22:09:31 +0900

    パンくずリストの設置

--- wp-content/themes/my-theme/includes/header.php
+++ wp-content/themes/my-theme/includes/header.php
@@ -3,3 +3,9 @@
     <h1><a class="title" href="<?php bloginfo('url'); ?>"><?php $title = get_bloginfo('name');
                                                             echo $title ?></a></h1>
 </header>
+<!-- パンくずリスト -->
+<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+    <?php if (function_exists('bcn_display')) {
+        bcn_display();
+    } ?>
+</div>
