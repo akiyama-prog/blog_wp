2ce7cf67680323018828e45e5e00684f063767e9
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-05-20 22:18:08 +0900

    タグの追加

--- wp-content/themes/my-theme/includes/nav.php
+++ wp-content/themes/my-theme/includes/nav.php
@@ -7,12 +7,13 @@
             <li class='nav-item'><a class="nav-link" href="/blog/category/programming">Programming</a></li>
             <li class='nav-item dropdown'><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id='navbarDropdown' aria-haspopup="true" aria-expanded="false" role="button">Travel</a>
                 <div class='dropdown-menu' aria-labelledby="navbarDropdown">
-                    <a class='dropdown-item' href="/blog/category/working-holiday/canada">Canada</a>
+                    <a class='dropdown-item' href="/blog/category/travel/canada">Canada</a>
                     <div class='dropdown-divider'></div>
-                    <a class='dropdown-item' href="/blog/category/working-holiday/australia">Australia</a>
+                    <a class='dropdown-item' href="/blog/category/travel/australia">Australia</a>
                 </div>
             </li>
-            <li class='nav-item'><a class="nav-link" href="/blog/category/goods">mono</a></li>
+            <li class='nav-item'><a class="nav-link" href="/blog/category/book">Book</a></li>
+            <li class='nav-item'><a class="nav-link" href="/blog/category/lifestyle">Life Style</a></li>
         </ul>
     </div>
 </nav>
