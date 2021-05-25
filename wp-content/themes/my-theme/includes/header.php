8713b5333ba9db93c3c726a7756d471a6a77d8d6
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-05-25 22:23:21 +0900

    タイトル追加

--- wp-content/themes/my-theme/includes/header.php
+++ wp-content/themes/my-theme/includes/header.php
@@ -1,7 +1,8 @@
 <header>
     <?php get_template_part('includes/nav'); ?>
     <h1><a class="title" href="<?php bloginfo('url'); ?>"><?php $title = get_bloginfo('name');
-                                                            echo $title ?></a></h1>
+                                                            echo $title ?><p>旅、本、プログラミング。</p></a></h1>
+
 </header>
 <!-- パンくずリスト -->
 <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
