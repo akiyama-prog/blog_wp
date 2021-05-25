a81070f2412e15a5ae951a9221418a4d29dac6a3
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-05-25 23:13:41 +0900

    レスポンスのヘッダーデザイン変更

--- wp-content/themes/my-theme/includes/header.php
+++ wp-content/themes/my-theme/includes/header.php
@@ -1,8 +1,12 @@
 <header>
     <?php get_template_part('includes/nav'); ?>
-    <h1><a class="title" href="<?php bloginfo('url'); ?>"><?php $title = get_bloginfo('name');
-                                                            echo $title ?><p>旅、本、プログラミング。</p></a></h1>
 
+    <?php if (wp_is_mobile()) : ?>
+        <h1><a class="title" href="<?php bloginfo('url'); ?>"><?php $title = get_bloginfo('name');
+                                                                echo $title ?><p>旅、本、<br>プログラミング。</p></a></h1>
+    <?php else : ?><h1><a class="title" href="<?php bloginfo('url'); ?>"><?php $title = get_bloginfo('name');
+                                                                            echo $title ?><p>旅、本、プログラミング。</p></a></h1>
+    <?php endif; ?>
 </header>
 <!-- パンくずリスト -->
 <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
