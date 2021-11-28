0e0162951b1ef3aac0e1dd6835006df7e4e89daa
akiyama-prog <autumn56y@gmail.com>
2021-11-28 13:50:12 +0900

    googleアドセンス

--- wp-content/themes/my-theme/header.php
+++ wp-content/themes/my-theme/header.php
@@ -1,3 +1,4 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6233576957841624" crossorigin="anonymous"></script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
