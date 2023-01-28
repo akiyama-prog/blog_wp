7fc8a5d0812d14f1543e65359024a6b12b548465
akiyama-prog <autumn56y@gmail.com>
2023-01-28 10:31:20 +0000

    スマホはアイキャッチ画像を削除

--- wp-content/themes/my-theme/includes/header.php
+++ wp-content/themes/my-theme/includes/header.php
@@ -1,9 +1,4 @@
     <?php if (wp_is_mobile()) : ?>
-        <h1 class="header">
-            <a class="title" href="<?php bloginfo('url'); ?>">
-                <span class="blog_title"><p>旅好きエンジニア</p><?php echo 'AKlog' ?></span>
-            </a>
-        </h1>
     <?php else : ?>
         <h1 class="header"><a class="title" href="<?php bloginfo('url'); ?>"><span class="blog_title"><p>旅好きエンジニア</p><?php $title = get_bloginfo('name');
                                                                                         echo $title ?></span>
